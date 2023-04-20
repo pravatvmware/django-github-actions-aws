1545020e9c80f6370cad17f9af5988688cd42183
root <root@MTLP-1287.in.mouritech.net>
2023-04-20 20:15:32 +0530

    v3

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 
 def home(request):
-   text = """<h1>Nice! This Appliaction is running on AWS EB environment V2</h1>"""
+   text = """<h1>Nice! This Appliaction is running on AWS EB environment V3</h1>"""
    return HttpResponse(text)
