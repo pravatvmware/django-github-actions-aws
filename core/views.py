b99974288b321058284076144298084ce05ae870
root <root@MTLP-1287.in.mouritech.net>
2023-04-20 19:14:45 +0530

    2nd commit

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 
 def home(request):
-   text = """<h1>our mind blowing home page</h1>"""
+   text = """<h1>Nice! This Appliaction is running on AWS EB environment</h1>"""
    return HttpResponse(text)
